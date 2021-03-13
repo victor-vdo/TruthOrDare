51373a4e1ae5095542cb536efbe4e7cfd1589731
Victor Oliveira <vitim@DESKTOP-8QPURBN>
2021-03-13 15:14:11 -0300

    Changing sdk version from Api  project

--- TruthOrDare.Api/Startup.cs
+++ TruthOrDare.Api/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,12 +25,7 @@ namespace TruthOrDare.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TruthOrDare.Api", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +34,6 @@ namespace TruthOrDare.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TruthOrDare.Api v1"));
             }
 
             app.UseHttpsRedirection();
